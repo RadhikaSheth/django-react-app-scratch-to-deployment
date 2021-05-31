280036d11027046e299da452e55a065c4eb2ac4a
RadhikaSheth <shethradhika9@gmail.com>
2021-05-31 10:05:29 +0530

    Solving MIME error

--- Wizard/views.py
+++ Wizard/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import Spell
 from .serializers import SpellSerializer
 from rest_framework.generics import ListAPIView,CreateAPIView
-
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os
 
 # Create your views here.
 
@@ -13,3 +15,14 @@ class AddSpell(ListAPIView):
 class GetSpell(CreateAPIView):
     queryset=Spell.objects.all()
     serializer_class = SpellSerializer
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
